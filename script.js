eaca81822ae1b8cf0bc8ede27e2ed51448a4f836
Fatima Ezzahra Legchayri <fatimaezzahra.legchayri@gmail.com>
2024-09-06 22:44:10 +0200

    add scroll animation

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ document.addEventListener('scroll',function(){
     header.classList.remove('header-shadow')
   }
 })
+
+
+
+
+const options = {
+  threshold : 0.4
+}
+const containerRight = document.querySelectorAll('.showRight-container')
+const observerRight = new IntersectionObserver(function(entries, observer){
+  entries.forEach(entry=>{
+    entry.target.classList.toggle('showRight', entry.isIntersecting)
+  })
+  
+},options)
+
+containerRight.forEach(el=>{
+  observerRight.observe(el)
+})
+
+
+const containerLeft = document.querySelectorAll('.showLeft-container')
+const observerLeft = new IntersectionObserver(function(entries, observer){
+  entries.forEach(entry=>{
+    entry.target.classList.toggle('showLeft', entry.isIntersecting)
+  })
+  
+},options)
+
+containerLeft.forEach(el=>{
+  observerLeft.observe(el)
+})
